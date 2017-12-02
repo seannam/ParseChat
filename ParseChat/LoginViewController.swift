19c0750d588906906a97bf4921e6306941df5f61
Sean Nam <namsangwoo1@gmail.com>
2017-12-02 11:46:12 -0800

    Added Parse user sign up

--- ParseChat/LoginViewController.swift
+++ ParseChat/LoginViewController.swift
@@ -7,9 +7,22 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        if !(usernameTextField.text?.isEmpty)! && !(passwordTextField.text?.isEmpty)! {
+            registerUser(username: usernameTextField.text!, password: passwordTextField.text!)
+        }
+    }
+    
+    @IBAction func onLogin(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +34,30 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func registerUser(username: String, password: String) {
+        // initialize a user object
+        let newUser = PFUser()
+        
+        // set user properties
+        newUser.username = username
+        newUser.password = password
+        
+        // call sign up function on the object
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+                // TODO: add alert message
+            } else {
+                print("User registered successfully")
+                // manually segue to logged in view
+                self.segueToLogin()
+            }
+        }
+    }
+    
+    func segueToLogin() {
+        
+    }
     /*
     // MARK: - Navigation
 
